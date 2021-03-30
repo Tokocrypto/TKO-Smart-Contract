f2d57cc48c38d1734928cb5a347cd2b7eaa85b0a
candra <candra@tokocrypto.com>
2021-03-30 22:55:28 +0700

    update burn method

--- contracts/TKOToken.sol
+++ contracts/TKOToken.sol
@@ -6,7 +6,7 @@ import "./libs/token/BEP20/BEP20.sol";
 
 // BEP20Token with Governance.
 // ('Token Name', 'Token Symbol', 'Token supply limit')
-contract TKOToken is BEP20('TKO Token', 'TKO', 500000000000000000000000000) {
+contract TKOToken is BEP20('TKO', 'TKO Token', 500000000000000000000000000) {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner.
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
@@ -14,7 +14,7 @@ contract TKOToken is BEP20('TKO Token', 'TKO', 500000000000000000000000000) {
     }
 
     function burn(address _from, uint256 _amount) public onlyOwner{
-        _burn(_from, _amount);
+        _burnFrom(_from, _amount);
         _moveDelegates(_delegates[_from], address(0), _amount);
     }
 
