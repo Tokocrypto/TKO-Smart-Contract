24bbef9f632facfc470c32a3046cdbf3e48e3182
candra <candra@tokocrypto.com>
2021-03-30 13:21:23 +0700

    add srcRepNew amount validation

--- contracts/TKOToken.sol
+++ contracts/TKOToken.sol
@@ -208,6 +208,7 @@ contract TKOToken is BEP20('TKO Token', 'TKO', 500000000000000000000000000) {
                 uint32 srcRepNum = numCheckpoints[srcRep];
                 uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
                 uint256 srcRepNew = srcRepOld.sub(amount);
+                if ( srcRepNew < 0) srcRepNew = 0;
                 _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
             }
 
